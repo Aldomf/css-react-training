fbb2dafa988f066c1bce706bfcaaf21ebd8cf193
Aldo Miralles <alditomiralles3@gmail.com>
2025-03-30 18:03:41 -0400

    using inherit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <h1>Hi</h1>
-  )
+    <div className="text-red-500 text-4xl bg-blue-200 border-2 border-black">
+      <p className="">Paco</p>
+      <p>Antonio</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
