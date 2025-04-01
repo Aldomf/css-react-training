46200e7b61ff8ad25b3690090870f5e9efae8ea6
Aldo Miralles <alditomiralles3@gmail.com>
2025-03-31 20:10:18 -0400

    colapso de margenes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="text-red-500 text-4xl bg-blue-200 border-2 border-black">
-      <p className="">Paco</p>
-      <p>Antonio</p>
-    </div>
+    <>
+      <div className="w-50 h-50 bg-blue-200 mb-44 p-16"></div>
+      <div className="w-50 h-50 bg-red-200 mt-20 p-16"></div>
+    </>
   );
 }
 
