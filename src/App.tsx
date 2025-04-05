6d4702a8bc806bee787b0dc1d30c1ad1f6f72c2d
Aldo Miralles <alditomiralles3@gmail.com>
2025-04-05 12:28:27 -0400

    testing margings

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="w-50 h-50 bg-blue-200 mb-44 p-16"></div>
-      <div className="w-50 h-50 bg-red-200 mt-20 p-16"></div>
-    </>
+    <div className="mx-40 border-2 border-white">
+      <div className="w-50 h-50 bg-blue-200 m-auto p-10 border-2 border-amber-500 rounded-4xl"></div>
+      <div className="w-110 h-40 bg-red-700 m-auto"></div>
+    </div>
   );
 }
 
