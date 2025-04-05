1f1b62e671a650b00f30445c69303976b903b7b3
Aldo Miralles <alditomiralles3@gmail.com>
2025-04-05 17:21:58 -0400

    testing positioning

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="mx-40 border-2 border-white">
-      <div className="w-50 h-50 bg-blue-200 m-auto p-10 border-2 border-amber-500 rounded-4xl"></div>
-      <div className="w-110 h-40 bg-red-700 m-auto"></div>
+    <div className="border-2 border-white h-svh flex flex-row items-center justify-center relative">
+      <div className="w-40 h-40 bg-blue-700 rounded-full inline-block absolute z-40 top-30"></div>
+      <div className="w-40 h-40 bg-red-700 inline-block absolute z-30 top-90">
+        <div className="absolute top-0 w-full text-center">Hello</div>
+      </div>
+      <div className="w-40 h-40 bg-green-700 rounded-full inline-block z-50"></div>
     </div>
   );
 }
