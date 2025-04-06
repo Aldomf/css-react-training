f26570413cd3dd8d5da4ac6f77812316a0222a18
Aldo Miralles <alditomiralles3@gmail.com>
2025-04-06 16:26:30 -0400

    using backdrop

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="border-2 border-white h-svh flex flex-row items-center justify-center relative">
-      <div className="w-40 h-40 bg-blue-700 rounded-full inline-block absolute z-40 top-30"></div>
-      <div className="w-40 h-40 bg-red-700 inline-block absolute z-30 top-90">
-        <div className="absolute top-0 w-full text-center">Hello</div>
-      </div>
-      <div className="w-40 h-40 bg-green-700 rounded-full inline-block z-50"></div>
+    <div className="border-2 border-white h-svh flex flex-col items-center justify-center relative gap-4 p-4 text-white">
+      <button
+        className="bg-blue-500 px-4 py-2 rounded"
+        onClick={() => document.getElementById("myDialog").showModal()}
+      >
+        Open Dialog
+      </button>
+
+      <dialog
+        id="myDialog"
+        className="p-8 rounded-lg text-black backdrop:bg-black/60 backdrop:backdrop-blur-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-48 bg-white"
+      >
+        <p className="text-lg font-bold">This is a dialog!</p>
+        <form
+          method="dialog"
+          className="flex flex-col justify-center items-center"
+        >
+          <button className="mt-4 bg-red-500 px-3 py-1 text-white rounded">
+            Close
+          </button>
+        </form>
+      </dialog>
     </div>
   );
 }
