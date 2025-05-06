175321d788b1306b2d13aba2eddd2977f2d3ec62
Aldo Miralles <alditomiralles3@gmail.com>
2025-05-05 20:55:55 -0400

    adding shadows

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
     <div className="flex justify-center items-center h-screen p-4 gap-6">
       <div className="group perspective-[1000px]">
         <a href="">
-          <figure className="w-[200px] h-72 rounded-2xl relative transition-all duration-500 ease-in transform-3d group-hover:rotate-y-180">
+          <figure className="w-[200px] h-72 rounded-2xl relative transition-all duration-500 ease-in transform-3d group-hover:rotate-y-180 group-hover:shadow-[6px_9px_10px_-2px_rgba(0,0,0,0.46)]
+">
             <img
               src="https://images.unsplash.com/photo-1544376798-89aa6b82c6cd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               className="rounded-2xl h-full w-full transition-all duration-500 ease-in"
