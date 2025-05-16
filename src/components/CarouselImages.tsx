259cb0a33359a4130cfc9b3a82c1ce25a10748e0
Aldo Miralles <alditomiralles3@gmail.com>
2025-05-16 18:39:59 -0400

    making carousel

--- src/components/CarouselImages.tsx
+++ src/components/CarouselImages.tsx
@@ -1,40 +1,21 @@
 function CarouselImages() {
   return (
-    <div className="w-full">
-      <div className=" content-carousel absolute">
-        <figure className="figure-carousel rotate-y-0 translate-z-[300px]">
-          <img src="img/img1.png" alt="image" className="w-100" />
-        </figure>
-        <figure className="figure-carousel rotate-y-40 translate-z-[300px]">
-          <img src="img/img2.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-80 translate-z-[300px]">
-          <img src="img/img3.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-120 translate-z-[300px]">
-          <img src="img/img4.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-160 translate-z-[300px]">
-          <img src="img/img5.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-200 translate-z-[300px]">
-          <img src="img/img6.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-240 translate-z-[300px]">
-          <img src="img/img7.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-280 translate-z-[300px]">
-          <img src="img/img8.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-320 translate-z-[300px]">
-          <img src="img/img9.png" alt="" className="w-100"/>
-        </figure>
-        <figure className="figure-carousel rotate-y-360 translate-z-[300px]">
-          <img src="img/img10.png" alt="" className="w-100"/>
-        </figure>
+    <div className="scene">
+      <div className="carousel">
+        {[...Array(10)].map((_, i) => (
+          <figure
+            key={i}
+            className="carousel__figure"
+            style={{ transform: `rotateY(${i * 45}deg) translateZ(400px)` }}
+          >
+            <img src={`img/img${i + 1}.png`} alt={`img${i + 1}`} />
+          </figure>
+        ))}
       </div>
     </div>
   );
 }
 
 export default CarouselImages;
+
+
